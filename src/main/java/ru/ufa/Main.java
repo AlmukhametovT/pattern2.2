a7342c9a39b91ca0ae590c92566c6b9ae75b92c9
Tagir Almukhametov <a.tagir@list.ru>
2022-07-08 23:40:19 +0500

    homework pattern 2.2 v1

---
+++ src/main/java/ru/ufa/Main.java
@@ -0,0 +1,11 @@
+package ru.ufa;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+
+        System.out.println(bins.sum("100", "101"));
+        System.out.println(bins.mult("100", "101"));
+    }
+}
